a687b482de99047ec96458e860c80dca90b650fa
Achal Varma <achalv2.0@gmail.com>
2013-11-26 19:56:10 -0600

    Fixed validation, thank god for regex.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
-var email = $("input.reg-email").val();
-$(".send").click(function() {  
-    $.ajax({  
-    type: "POST",  
-    url: "/",  
-    data: email,  
-    success: function() {  
-        $(".reg-form").fadeOut( "slow", function() {
-            $(".reg-container").append("<h3>You're all set! We'll be in touch soon.</h3>");
-        });
-    }  
-    });  
-return false;   
+$( document ).ready(function() {
+    $(".reg-form").submit(function () {
+        var email = $("input.reg-email").val();
+        var email_check = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i;
+        if (!email_check.test(email)) {
+            $('.reg-email').addClass("email-error");
+            return false;
+        } 
+        else {
+            $.ajax({
+                type: "POST",
+                url: "",
+                data: email,
+                success: function () {
+                    $(".reg-form").fadeOut("slow", function () {
+                        $(".reg-container").append("<h3>You're all set! We'll be in touch soon.</h3>");
+                    });
+                }
+            });
+            return false;
+        }
+    });
 });
 
