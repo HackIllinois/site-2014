a5823dc0235960f905e8a4435e5eaa506b2db6df
Ace Nassri <ace.nassri@gmail.com>
2014-01-30 01:11:51 -0600

    Added code of conduct page

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -3,7 +3,7 @@ from ErrorHandler import ErrorHandler
 from IndexHandler import IndexHandler
 from RegisterHandler import RegisterHandler, RegisterCompleteHandler
 from SignupCountHandler import SignupCountHandler
-from SubpageHandlers import RulesHandler, ScheduleHandler, TravelHandler
+from SubpageHandlers import RulesHandler, ScheduleHandler, TravelHandler, CoCHandler
 from TropoHandler import TropoHandler
 import ProfileHandler
 # import AdminHandler
@@ -21,6 +21,7 @@ handlers = [
     ('/travel', TravelHandler),
     ('/tropo', TropoHandler),
     ('/profile', ProfileHandler.ProfileHandler),
+    ('/code-of-conduct', CoCHandler),
     # ('/admin', AdminHandler.AdminHandler),
     # ('/_ah/login_required', LoginRequiredHandler),
     ('.*', ErrorHandler)
