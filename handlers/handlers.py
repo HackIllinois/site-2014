cc4c247c7175bf0bfb7129c844d922d619aea689
Will Hennessy <hennessywill@gmail.com>
2014-02-05 20:36:52 -0600

    fixed merge issue in handlers.py

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -5,13 +5,9 @@ from RegisterHandler import RegisterHandler, RegisterCompleteHandler, SchoolChec
 from SignupCountHandler import SignupCountHandler
 from SubpageHandlers import RulesHandler, ScheduleHandler, TravelHandler, CoCHandler
 from TropoHandler import TropoHandler
-<<<<<<< HEAD
 from AdminHandler import AdminHandler
 from ApproveHandler import ApproveHandler
-import ProfileHandler
-=======
 from ProfileHandler import ProfileHandler, MyResumeHandler
->>>>>>> ef69ad9a0b1bd66fe4248e86e34eb227dc70402b
 # import AdminHandler
 # from LoginRequiredHandler import LoginRequiredHandler
 
@@ -27,16 +23,12 @@ handlers = [
     ('/signupcount', SignupCountHandler),
     ('/travel', TravelHandler),
     ('/tropo', TropoHandler),
-<<<<<<< HEAD
     ('/profile', ProfileHandler.ProfileHandler),
     ('/admin', AdminHandler),
     ('/admin/approve', ApproveHandler),
-=======
     ('/profile', ProfileHandler),
     ('/profile/myresume', MyResumeHandler),
     ('/code-of-conduct', CoCHandler),
-    # ('/admin', AdminHandler.AdminHandler),
->>>>>>> ef69ad9a0b1bd66fe4248e86e34eb227dc70402b
     # ('/_ah/login_required', LoginRequiredHandler),
     ('.*', ErrorHandler)
 ]
