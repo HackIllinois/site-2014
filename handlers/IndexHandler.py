dd10b07a2361ce0a7d7dbe345876c8b82b132d03
Achal Varma <achalv2.0@gmail.com>
2014-02-09 22:56:19 -0600

    Refactored the way Update/Apply button is displayed on homepage depending on login.

--- handlers/IndexHandler.py
+++ handlers/IndexHandler.py
@@ -5,16 +5,13 @@ from google.appengine.api import users
 class IndexHandler(MainHandler.Handler):
     """ Handler for the homepage, """
     def get(self):
-        buttonText = 'Apply'
-        addedClassesForLoggedInUsers = ''
-
+        sliderButtonClass = 'apply-info'
+        updateProfile = False
         user = users.get_current_user()
         
         if user:
             db_user = Attendee.search_database({'userId': user.user_id()}).get()
             if db_user and db_user.isRegistered:
-                buttonText = 'Update Profile'
-                addedClassesForLoggedInUsers = 'update-btn' 
-                # Above line adds classes for fixing button size if person is updating application. Sorry about the long but descriptive var name.
+                updateProfile = True                    
 
-        self.render("index.html", buttonText=buttonText, addedClassesForLoggedInUsers=addedClassesForLoggedInUsers)
+        self.render("index.html", updateProfile=updateProfile, sliderButtonClass=sliderButtonClass)
