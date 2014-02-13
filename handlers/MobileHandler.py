d2726e57c435ec2f073b0e87aa467286204908d6
Jacob <jacobfuss4@gmail.com>
2014-02-13 00:55:06 -0600

    setup endpoints in handlers and basic outline in MobileHandlers

--- handlers/MobileHandler.py
+++ handlers/MobileHandler.py
@@ -5,3 +5,82 @@ from db import constants
 from google.appengine.api import users, memcache
 import json
 
+class MobileHandler:
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class ScheduleHandler:
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class MapsHandler:
+    
+    def get(self):
+        pass
+    
+    def post(self):
+        pass
+
+class SupportTypeHandler:
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class EmergencyHandler:
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class NewsfeedHandler:
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class StaffHandler:
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class HackersHandler:
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class CompanyHandler:
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+class SkillsHandler:
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
