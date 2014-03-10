a9a8742f16ce1109ef076d989de37b3b0e46330c
Austin Hampton <austinh100@yahoo.com>
2014-03-10 17:52:49 -0500

    added a zip function for directories

--- backend_tasks/worker_functions.py
+++ backend_tasks/worker_functions.py
@@ -1,9 +1,28 @@
 import requests
+import zipfile, os
 
 """
 All Task functions go here, to be executed from the queue and Rq workers
 """
 
+#remove, not needed, just a placeholder
 def count_words_at_url(url):
     resp = requests.get(url)
     return len(resp.text.split())
+
+#zips a full directory
+def toZip(directory):
+    zippedHelp = zipfile.ZipFile(zip, "w", compression=zipfile.ZIP_DEFLATED )
+ 
+    list = os.listdir(directory)
+ 
+    for entity in list:
+        each&nbsp;= os.path.join(directory,entity)
+ 
+        if&nbsp;os.path.isfile(each):
+            print each
+            zippedHelp.write(each,zipfile.ZIP_DEFLATED)
+        else:
+            addFolderToZip(zippedHelp,entity)
+ 
+    zippedHelp.close()
