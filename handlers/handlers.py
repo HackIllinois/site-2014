5a93cc7422bcba904ef7a80b7e561da96d1255b0
Alex Burck <burck1@illinois.edu>
2014-03-12 17:17:33 -0500

    fixed handler imports

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,52 +1,53 @@
 from webapp2_extras.routes import RedirectRoute
 
 from EmailBackupHandler import EmailBackupHandler
-from ErrorHandlers import handle_401, handle_404, handle_500, Error401Handler, Error404Handler, Error500Handler
 from IndexHandler import IndexHandler
-from ApplyHandler import ApplyHandler, ApplyCompleteHandler, SchoolCheckHandler, SchoolListHandler, MyResumeHandler, UpdateCompleteHandler, UploadURLHandler
 from ApplyCountHandler import ApplyCountHandler
-import SubpageHandlers
-from SubpageHandlers import RulesHandler, ScheduleHandler, TravelHandler, CoCHandler, StaffHandler, SponsorFAQHandler
 from TropoHandler import TropoHandler
-from AdminHandler import AdminHandler, AdminApproveHandler, AdminResumeHandler, AdminStatsHandler, AdminBasicStatsHandler, AdminXkcdHandler, AdminProfileHandler, AdminEditProfileHandler, AdminApplyCountHandler, AdminSchoolCountHandler
 from SponsorHandler import SponsorDownloadHandler
 from LogoutHandler import LogoutHandler
 from MGTHandler import MGTHandler, ParticlesHandler
 from TestAllHandler import TestAllHandler, TestAllJsHandler
+import ApplyHandler
+import SubpageHandlers
+import AdminHandler
 import MobileHandler
+import ErrorHandlers
 
 handlers = [
     RedirectRoute('/', handler=IndexHandler, name='Index', strict_slash=True),
     RedirectRoute('/emailbackup', handler=EmailBackupHandler, name='EmailBackup', strict_slash=True),
 
-    RedirectRoute('/apply', handler=ApplyHandler, name='Apply', strict_slash=True),
-    RedirectRoute('/apply/complete', handler=ApplyCompleteHandler, name='ApplyComplete', strict_slash=True),
-    RedirectRoute('/apply/updated', handler=UpdateCompleteHandler, name='UpdateComplete', strict_slash=True),
-    RedirectRoute('/apply/schoolcheck', handler=SchoolCheckHandler, name='SchoolCheck', strict_slash=True),
-    RedirectRoute('/apply/schoollist', handler=SchoolListHandler, name='SchoolList', strict_slash=True),
-    RedirectRoute('/apply/myresume', handler=MyResumeHandler, name='MyResume', strict_slash=True),
-    RedirectRoute('/apply/uploadurl', handler=UploadURLHandler, name='UploadURL', strict_slash=True),
+    RedirectRoute('/apply', handler=ApplyHandler.ApplyHandler, name='Apply', strict_slash=True),
+    RedirectRoute('/apply/complete', handler=ApplyHandler.ApplyCompleteHandler, name='ApplyComplete', strict_slash=True),
+    RedirectRoute('/apply/updated', handler=ApplyHandler.UpdateCompleteHandler, name='UpdateComplete', strict_slash=True),
+    RedirectRoute('/apply/schoolcheck', handler=ApplyHandler.SchoolCheckHandler, name='SchoolCheck', strict_slash=True),
+    RedirectRoute('/apply/schoollist', handler=ApplyHandler.SchoolListHandler, name='SchoolList', strict_slash=True),
+    RedirectRoute('/apply/myresume', handler=ApplyHandler.MyResumeHandler, name='MyResume', strict_slash=True),
+    RedirectRoute('/apply/uploadurl', handler=ApplyHandler.UploadURLHandler, name='UploadURL', strict_slash=True),
+
     RedirectRoute('/applycount', handler=ApplyCountHandler, name='ApplyCount', strict_slash=True),
 
-    RedirectRoute('/rules', handler=RulesHandler, name='Rules', strict_slash=True),
+    RedirectRoute('/rules', handler=SubpageHandlers.RulesHandler, name='Rules', strict_slash=True),
     RedirectRoute('/schedule', handler=SubpageHandlers.ScheduleHandler, name='Schedule', strict_slash=True),
-    RedirectRoute('/travel', handler=TravelHandler, name='Travel', strict_slash=True),
-    RedirectRoute('/sponsor/faq', handler=SponsorFAQHandler, name='SponsorFAQ', strict_slash=True),
-    RedirectRoute('/tropo', handler=TropoHandler, name='Tropo', strict_slash=True),
-    RedirectRoute('/code-of-conduct', handler=CoCHandler, name='CoC', strict_slash=True),
+    RedirectRoute('/travel', handler=SubpageHandlers.TravelHandler, name='Travel', strict_slash=True),
+    RedirectRoute('/sponsor/faq', handler=SubpageHandlers.SponsorFAQHandler, name='SponsorFAQ', strict_slash=True),
+    RedirectRoute('/tropo', handler=SubpageHandlers.TropoHandler, name='Tropo', strict_slash=True),
+    RedirectRoute('/code-of-conduct', handler=SubpageHandlers.CoCHandler, name='CoC', strict_slash=True),
 
     RedirectRoute('/sponsor/download', handler=SponsorDownloadHandler, name='SponsorDownload', strict_slash=True),
 
-    RedirectRoute('/admin', handler=AdminHandler, name='Admin', strict_slash=True),
-    RedirectRoute('/admin/xkcd', handler=AdminXkcdHandler, name='AdminXkcd', strict_slash=True),
-    RedirectRoute('/admin/approve', handler=AdminApproveHandler, name='AdminApprove', strict_slash=True),
-    RedirectRoute('/admin/stats', handler=AdminStatsHandler, name='AdminStats', strict_slash=True),
-    RedirectRoute('/admin/basicstats', handler=AdminBasicStatsHandler, name='AdminBasicStats', strict_slash=True),
-    RedirectRoute('/admin/resume', handler=AdminResumeHandler, name='AdminResume', strict_slash=True),
-    RedirectRoute('/admin/applycount', handler=AdminApplyCountHandler, name='AdminApplyCount', strict_slash=True),
-    RedirectRoute('/admin/schoolcount', handler=AdminSchoolCountHandler, name='AdminSchoolCount', strict_slash=True),
-    RedirectRoute('/admin/profile/<userId>', handler=AdminProfileHandler, name='AdminProfile', strict_slash=True),
-    RedirectRoute('/admin/profile/<userId>/edit', handler=AdminEditProfileHandler, name='AdminEditProfile', strict_slash=True),
+    RedirectRoute('/admin', handler=AdminHandler.AdminHandler, name='Admin', strict_slash=True),
+    RedirectRoute('/admin/xkcd', handler=AdminHandler.AdminXkcdHandler, name='AdminXkcd', strict_slash=True),
+    RedirectRoute('/admin/approve', handler=AdminHandler.AdminApproveHandler, name='AdminApprove', strict_slash=True),
+    RedirectRoute('/admin/stats', handler=AdminHandler.AdminStatsHandler, name='AdminStats', strict_slash=True),
+    RedirectRoute('/admin/basicstats', handler=AdminHandler.AdminBasicStatsHandler, name='AdminBasicStats', strict_slash=True),
+    RedirectRoute('/admin/resume', handler=AdminHandler.AdminResumeHandler, name='AdminResume', strict_slash=True),
+    RedirectRoute('/admin/applycount', handler=AdminHandler.AdminApplyCountHandler, name='AdminApplyCount', strict_slash=True),
+    RedirectRoute('/admin/schoolcount', handler=AdminHandler.AdminSchoolCountHandler, name='AdminSchoolCount', strict_slash=True),
+    RedirectRoute('/admin/profile/<userId>', handler=AdminHandler.AdminProfileHandler, name='AdminProfile', strict_slash=True),
+    RedirectRoute('/admin/profile/<userId>/edit', handler=AdminHandler.AdminEditProfileHandler, name='AdminEditProfile', strict_slash=True),
+
     RedirectRoute('/admin/tests', handler=TestAllHandler, name='TestAll', strict_slash=True),
     RedirectRoute('/admin/testsjs', handler=TestAllJsHandler, name='TestAllJs', strict_slash=True),
 
@@ -63,17 +64,17 @@ handlers = [
 
     RedirectRoute('/logout', handler=LogoutHandler, name='Logout', strict_slash=True),
 
-    # RedirectRoute('/hidden/staff', handler=StaffHandler, name='Staff', strict_slash=True),
+    # RedirectRoute('/hidden/staff', handler=SubpageHandlers.StaffHandler, name='Staff', strict_slash=True),
     RedirectRoute('/mgt', handler=MGTHandler, name='MGT', strict_slash=True),
     RedirectRoute('/particles', handler=ParticlesHandler, name='Particles', strict_slash=True),
 
-    RedirectRoute('/401', handler=Error401Handler, name='Error401', strict_slash=True),
-    RedirectRoute('/404', handler=Error404Handler, name='Error404', strict_slash=True),
-    RedirectRoute('/500', handler=Error500Handler, name='Error500', strict_slash=True),
+    RedirectRoute('/401', handler=ErrorHandlers.Error401Handler, name='Error401', strict_slash=True),
+    RedirectRoute('/404', handler=ErrorHandlers.Error404Handler, name='Error404', strict_slash=True),
+    RedirectRoute('/500', handler=ErrorHandlers.Error500Handler, name='Error500', strict_slash=True),
 ]
 
 errorHandlers = {
-    401: handle_401,
-    404: handle_404,
-    500: handle_500,
+    401: ErrorHandlers.handle_401,
+    404: ErrorHandlers.handle_404,
+    500: ErrorHandlers.handle_500,
 }
