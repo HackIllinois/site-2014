6b6912024e9d0762e2bf67a12d1bf8c92986d92d
Alex Burck <burck1@illinois.edu>
2014-03-21 21:34:28 -0500

    Updated home page to say Update Application instead of Apply

--- handlers/IndexHandler.py
+++ handlers/IndexHandler.py
@@ -10,32 +10,6 @@ from db.Attendee import Attendee
 from google.appengine.api import users
 
 class IndexHandler(MainHandler.Handler):
-    ## Handler for the homepage
     def get(self):
         data = {}
-        user = users.get_current_user()
-        data['isLoggedIn'] = False
-        if user is not None:
-            db_user = Attendee.search_database({'userId': user.user_id()}).get()
-            if db_user is not None and db_user.isRegistered:
-                  data['isLoggedIn'] = True
-    
         self.render("index.html", data=data)
-
-    def post(self):
-        """ Handle email signups """
-        email = self.request.body
-
-        buttonText = 'Apply'
-        addedClassesForLoggedInUsers = ''
-
-        sliderButtonClass = 'apply-info'
-        updateProfile = False
-        user = users.get_current_user()
-        
-        if user:
-            db_user = Attendee.search_database({'userId': user.user_id()}).get()
-            if db_user and db_user.isRegistered:
-                updateProfile = True                    
-
-        self.render("index.html", updateProfile=updateProfile, sliderButtonClass=sliderButtonClass)
