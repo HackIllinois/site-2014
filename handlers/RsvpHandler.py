f6596bb3a4aa6385e5e2b77f9bb6738a0e3ef380
Ben Fishbein <fishbeinb@gmail.com>
2014-03-26 08:57:20 -0500

    more stuff, battery dying

--- handlers/RsvpHandler.py
+++ handlers/RsvpHandler.py
@@ -2,11 +2,13 @@ import urllib
 import logging
 import re
 import json
+from datetime import datetime
 
 import MainHandler
 
 from db.Attendee import Attendee
 from db.Resume import Resume
+from db.Status import Status
 from db import constants
 from google.appengine.api import users
 from google.appengine.ext import blobstore
@@ -36,10 +38,44 @@ class RsvpHandler(MainHandler.Handler):
 		
         data['username'] = db_user.userNickname
         data['email'] = db_user.email
-        data['uploadUrl'] = blobstore.create_upload_url('/apply/update', gs_bucket_name=constants.BUCKET)
+        data['uploadUrl'] = blobstore.create_upload_url('/apply/rsvp', gs_bucket_name=constants.BUCKET)
         data['logoutUrl'] = users.create_logout_url('/apply')
 		
         self.render("rsvp.html", data=data)
 
     def post(self):
+        user = users.get_current_user()
+        if not user: return self.abort(500, detail='User not logged in')
+
+        db_user = Attendee.search_database({'userId':user.user_id()}).get()
+
+        # Login Page
+        if db_user is None:
+		    return self.redirect('/apply/closed')
+
+        if db_user is not None and (db_user.isRegistered == False):
+            return self.redirect('/apply/closed')
+			
+		#Not yet approved page
+        if db_user is not None and (db_user.isApproved == False):
+            return self.redirect('/apply/closed')
+			
+        # Initialization
+        x = {} # dictionary that will be used to create a new Attendee or update one
+        errors = {} # Note: 1 error per field
+        valid = True
+		
+        #foods = self.request.get_all('food')
+        #x['food'] = ','.join(foods)
+        
+        x['userId'] = user.user_id() 
+        x['approved'] = 'aaaaa'
+        
+        x['approvalStatus'] = Status(rsvpTime=datetime.now(),
+                                     waitlistedTime=db_user.approvalStatus.waitlistedTime,
+                                     approvedTime=db_user.approvalStatus.approvedTime)
+
+        Attendee.update_search(x, {'userId':x['userId']})
+        
+		
         pass
