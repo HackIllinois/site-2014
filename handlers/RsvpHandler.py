3e6acfd85eee09082673c230499536c665e716c3
Ben Fishbein <fishbeinb@gmail.com>
2014-03-27 23:03:48 -0500

    more RSVP

--- handlers/RsvpHandler.py
+++ handlers/RsvpHandler.py
@@ -44,6 +44,9 @@ class RsvpHandler(MainHandler.Handler):
         data['username'] = db_user.userNickname
         data['email'] = db_user.email
 
+        if db_user.approvalStatus:
+            data['status'] = db_user.approvalStatus.status
+		
         lists = {
             'micro1':constants.MICRO1, 'micro2':constants.MICRO2
             }
@@ -117,11 +120,16 @@ class RsvpHandler(MainHandler.Handler):
         isAttending = self.request.get("attend")
 		
         if isAttending == "Yes":
-            x['approvalStatus'] = Status(rsvpTime=datetime.now(),
-                waitlistedTime=db_user.approvalStatus.waitlistedTime,
-                approvedTime=db_user.approvalStatus.approvedTime,
-                emailedTime=db_user.approvalStatus.emailedTime,
-                status = 'Rsvp Coming')
+            if db_user.approvalStatus:
+                x['approvalStatus'] = Status(rsvpTime=datetime.now(),
+                    waitlistedTime=db_user.approvalStatus.waitlistedTime,
+                    approvedTime=db_user.approvalStatus.approvedTime,
+                    emailedTime=db_user.approvalStatus.emailedTime,
+                    status = 'Rsvp Coming')
+            else:
+                x['approvalStatus'] = Status(rsvpTime=datetime.now(),
+                    status = 'Rsvp Coming')
+					
             categories = ['micro1', 'micro2', 'labEquipment']
             for category in categories:
                 list = self.request.get_all(category)
@@ -129,29 +137,43 @@ class RsvpHandler(MainHandler.Handler):
 
 			
             x['travel'] = self.request.get('travel')
-            x['busRoute'] = self.request.get('busRoute')
+            if x['travel'] == 'I would like to ride a HackIllinois bus':
+                x['busRoute'] = self.request.get('busRoute')
+            else:
+                x['busRoute'] = None
+
         # ---------- BEGIN VALIDATION ----------
-			
             for m1 in x['micro1'].split(','):
-                if m1 not in constants.MICRO1:
+                micro1Flag = True
+                for temp in constants.MICRO1:
+                    if temp[0] == m1 or not m1:
+                        micro1Flag = False
+                if micro1Flag:
                     errors['micro1'] = "Invalid Microcontrollers"
                     valid = False
                     break
+            
             for m2 in x['micro2'].split(','):
-                if m2 not in constants.MICRO2:
+                micro2Flag = True
+                for temp in constants.MICRO2:
+                    if temp[0] == m2 or not m2:
+                        micro2Flag = False
+                if micro2Flag:
                     errors['micro2'] = "Invalid Microcontrollers"
                     valid = False
                     break
+            
+            
             for l in x['labEquipment'].split(','):
-                if l not in constants.LABEQUIPMENT:
+                if l not in constants.LABEQUIPMENT and l:
                     errors['labEquipment'] = "Invalid Lab Equipment"
                     valid = False
                     break
 			
-            if x['travel'] or x['travel'] == constants.TRAVEL_NO_RESPONSE:
+            if not x['travel'] or x['travel'] == constants.TRAVEL_NO_RESPONSE:
                errors['travel'] = "Provide Travel Plans"
                valid = False
-			   
+
             if not x['travel'] and (x['travel'] not in constants.TRAVEL_ARRANGEMENTS):
                errors['travel'] = "Invalid Travel Plans"
                valid = False
@@ -160,21 +182,24 @@ class RsvpHandler(MainHandler.Handler):
                 if ('busRoute' in x and x['busRoute']) or x['busRoute'] not in constatns.BUS_ROUTES:
                     errors['busRoute'] = "Invalid Bus Route"
                     valid = False
-			
+
         # ---------- END VALIDATION ----------
-		
 			
         else:
-            x['approvalStatus'] = Status(rsvpTime=datetime.now(),
-                waitlistedTime=db_user.approvalStatus.waitlistedTime,
-                approvedTime=db_user.approvalStatus.approvedTime,
-                emailedTime=db_user.approvalStatus.emailedTime,
-                status = 'Rsvp Not Coming')
-
-        Attendee.update_search(x, {'userId':x['userId']})
-
-
-        return self.redirect("/apply")
+            if db_user.approvalStatus:
+                x['approvalStatus'] = Status(rsvpTime=datetime.now(),
+                    waitlistedTime=db_user.approvalStatus.waitlistedTime,
+                    approvedTime=db_user.approvalStatus.approvedTime,
+                    emailedTime=db_user.approvalStatus.emailedTime,
+                    status = 'Rsvp Not Coming')
+            else:
+                x['approvalStatus'] = Status(rsvpTime=datetime.now(),
+                    status = 'Rsvp Not Coming')
+        if valid:
+            Attendee.update_search(x, {'userId':x['userId']})
+            return self.redirect("/apply")
+        else:
+            return self.redirect("/apply")
 
 class NotApprovedHandler(MainHandler.Handler):
     def get(self):
