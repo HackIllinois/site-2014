d77013f9095f5546aaaa62c9e0c7854e16b17a57
Alex Burck <burck1@illinois.edu>
2014-03-28 20:10:39 -0500

    wrong way

--- handlers/RsvpHandler.py
+++ handlers/RsvpHandler.py
@@ -30,10 +30,10 @@ class RsvpHandler(MainHandler.Handler):
         if db_user.approvalStatus is None:
             return self.redirect('/rsvp/pending')
 
-        if db_user.approvalStatus.status == 'Rsvp Not Coming':
+        if db_user.approvalStatus.status == 'No Rsvp':
             return self.redirect('/rsvp/closed')
 
-        if db_user.approvalStatus.status == 'No Rsvp':
+        if db_user.approvalStatus.status == 'Rsvp Not Coming':
             return self.redirect('/rsvp/no')
 
         if db_user.approvalStatus.status not in ['Awaiting Response', 'Rsvp Coming']:
@@ -103,10 +103,10 @@ class RsvpHandler(MainHandler.Handler):
         if db_user.approvalStatus is None:
             return self.redirect('/rsvp/pending')
 
-        if db_user.approvalStatus.status == 'Rsvp Not Coming':
+        if db_user.approvalStatus.status == 'No Rsvp':
             return self.redirect('/rsvp/closed')
 
-        if db_user.approvalStatus.status == 'No Rsvp':
+        if db_user.approvalStatus.status == 'Rsvp Not Coming':
             return self.redirect('/rsvp/no')
 
         if db_user.approvalStatus.status not in ['Awaiting Response', 'Rsvp Coming']:
