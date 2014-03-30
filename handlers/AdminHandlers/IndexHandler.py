1cb1654e1be90b758ff087b7a61f2a4bc97bfd82
Matthew Dierker <mvd7793@gmail.com>
2014-03-30 14:02:32 -0500

    briefly break the admin index to double check that our duplicate email problem is fixed

--- handlers/AdminHandlers/IndexHandler.py
+++ handlers/AdminHandlers/IndexHandler.py
@@ -1,5 +1,7 @@
 import MainAdminHandler
 from db import constants
+from db.Attendee import Attendee
+
 
 class IndexHandler(MainAdminHandler.BaseAdminHandler):
     def get(self):
@@ -7,12 +9,22 @@ class IndexHandler(MainAdminHandler.BaseAdminHandler):
         if not admin_user: return self.abort(500, detail='User not in database')
 
         data = {}
-        data['registerCount'] = self.get_apply_count_memcache(constants.USE_ADMIN_MEMCACHE)
-        data['schoolCount'] = self.get_school_count_memcache(constants.USE_ADMIN_MEMCACHE)
-        data['approveCount'] = self.get_status_count_memcache('Approved', constants.USE_ADMIN_MEMCACHE)
-        data['waitlistCount'] = self.get_status_count_memcache('Waitlisted', constants.USE_ADMIN_MEMCACHE)
-        data['emailedCount'] = self.get_status_count_memcache('Awaiting Response', constants.USE_ADMIN_MEMCACHE)
-        data['rsvpYesCount'] = self.get_status_count_memcache('Rsvp Coming', constants.USE_ADMIN_MEMCACHE)
-        data['rsvpNoCount'] = self.get_status_count_memcache('Rsvp Not Coming', constants.USE_ADMIN_MEMCACHE)
+        # data['registerCount'] = self.get_apply_count_memcache(constants.USE_ADMIN_MEMCACHE)
+        # data['schoolCount'] = self.get_school_count_memcache(constants.USE_ADMIN_MEMCACHE)
+        # data['approveCount'] = self.get_status_count_memcache('Approved', constants.USE_ADMIN_MEMCACHE)
+        # data['waitlistCount'] = self.get_status_count_memcache('Waitlisted', constants.USE_ADMIN_MEMCACHE)
+        # data['emailedCount'] = self.get_status_count_memcache('Awaiting Response', constants.USE_ADMIN_MEMCACHE)
+        # data['rsvpYesCount'] = self.get_status_count_memcache('Rsvp Coming', constants.USE_ADMIN_MEMCACHE)
+        # data['rsvpNoCount'] = self.get_status_count_memcache('Rsvp Not Coming', constants.USE_ADMIN_MEMCACHE)
 
-        return self.render('admin.html', data=data, approveAccess=admin_user.approveAccess, fullAccess=admin_user.fullAccess)
+        dup_hackers = Attendee.query(Attendee.isRegistered == True)
+
+        map = {}
+        for hacker in dup_hackers:
+            if hacker.email in map:
+                print hacker.email
+            else:
+                map[hacker.email] = [hacker]
+
+        return self.render('admin.html', data=data, approveAccess=admin_user.approveAccess,
+                           fullAccess=admin_user.fullAccess)
