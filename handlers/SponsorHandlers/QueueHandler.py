ac8094b7f6609a0079b43a63e62f836888033792
Alex Burck <burck1@illinois.edu>
2014-04-08 18:04:22 -0500

    created queue page

--- handlers/SponsorHandlers/QueueHandler.py
+++ handlers/SponsorHandlers/QueueHandler.py
@@ -1,11 +1,19 @@
 import MainSponsorHandler
 from db import constants
 import json
+from db.Task import Task
 
 class QueueHandler(MainSponsorHandler.BaseSponsorHandler):
     def get(self):
-        me = self.db_user
-
-        data = {}
-        # return self.render('admin.html', data=data, access=json.loads(admin_user.access))
-        return self.render('sponsor/queue.html', data=data, access=json.loads(self.db_user.access))
+        tasks = []
+        for t in self.db_user.task_queue:
+            task = t.get()
+            tasks.append({
+                'complete':task.complete,
+                'id':task.key.id(),
+                'url':'http://23.236.61.209/serve/' + str(task.key.id()),
+                'creationTime':task.creationTime.strftime('%x %X'),
+                'completeTime':task.completeTime.strftime('%x %X'),
+                'errorMessages':task.errorMessages,
+            })
+        return self.render('sponsor/queue.html', tasks=tasks, access=json.loads(self.db_user.access))
