0753a7aef99a095a7e605bab332c55a0586e7ce2
Alex Burck <burck1@illinois.edu>
2014-04-08 18:42:00 -0500

    small edits

--- handlers/SponsorHandlers/QueueHandler.py
+++ handlers/SponsorHandlers/QueueHandler.py
@@ -2,18 +2,30 @@ import MainSponsorHandler
 from db import constants
 import json
 from db.Task import Task
+from operator import itemgetter
 
 class QueueHandler(MainSponsorHandler.BaseSponsorHandler):
     def get(self):
         tasks = []
         for t in self.db_user.task_queue:
             task = t.get()
+            if not task: continue
+
+            errors = task.errorMessages
+            errors = errors[1:]
+            errors = errors[:-1]
+            if len(errors) > 50: errors = errors[:50] + '...'
+
             tasks.append({
                 'complete':task.complete,
                 'id':task.key.id(),
                 'url':'http://23.236.61.209/serve/' + str(task.key.id()),
                 'creationTime':task.creationTime.strftime('%x %X'),
                 'completeTime':task.completeTime.strftime('%x %X'),
-                'errorMessages':task.errorMessages,
+                'errorMessages':errors,
+                'sort':task.creationTime,
             })
+
+        tasks = sorted(tasks, key=itemgetter('sort'), reverse=True)
+
         return self.render('sponsor/queue.html', tasks=tasks, access=json.loads(self.db_user.access))
