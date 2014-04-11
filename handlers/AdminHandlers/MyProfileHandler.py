92729fb7b64f62dae05297b687b8458b29220af2
Alex Burck <burck1@illinois.edu>
2014-04-11 00:22:05 -0500

    a bunch of stuff

--- handlers/AdminHandlers/MyProfileHandler.py
+++ handlers/AdminHandlers/MyProfileHandler.py
@@ -1,10 +1,13 @@
-import MainSponsorHandler
+import MainAdminHandler
 import urllib, logging, json
-from db.Sponsor import Sponsor
+from db.Admin import Admin
 from db import constants
 
-class EditProfileHandler(MainSponsorHandler.BaseSponsorHandler):
+class MyProfileHandler(MainAdminHandler.BaseAdminHandler):
     def get(self):
+        admin_user = self.get_admin_user()
+        if not admin_user: return self.abort(500, detail='User not in database')
+
         data = {}
         text_fields = ['email', 'companyName', 'name', 'userEmail', 'jobTitle']
 
@@ -12,11 +15,11 @@ class EditProfileHandler(MainSponsorHandler.BaseSponsorHandler):
             value = getattr(self.db_user, field) # Gets db_user.field using a string
             if value is not None: data[field] = value
 
-        return self.render("sponsor/edit_profile.html", data=data, access=json.loads(self.db_user.access))
+        return self.render('admin_my_profile.html', data=data, access=json.loads(admin_user.access))
 
     def post(self):
-        self.db_user = self.get_sponsor_user()
-        if not self.db_user: return self.abort(500, detail='User not in database')
+        admin_user = self.get_admin_user()
+        if not admin_user: return self.abort(500, detail='User not in database')
 
         name = str(urllib.unquote(self.request.get('name')))
         companyName = str(urllib.unquote(self.request.get('companyName')))
