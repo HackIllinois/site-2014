afa94b487a9a90413927690ce4614ba7b772e5e5
bjryan2 <bjryan2@illinois.edu>
2014-04-11 02:11:23 -0500

    typeaheads and correct rendering

--- assets/js/admin_schedule.js
+++ assets/js/admin_schedule.js
@@ -1,3 +1,40 @@
+var rooms = [{'building':'Siebel', 'floor':'Basement', 'room_number':'SC 0216', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'Basement', 'room_number':'SC 0224', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'Basement', 'room_number':'SC Basement', 'room_type':'hacker space' ,'room_name':'Basement Open Space', 'image_url':''},
+{'building':'Siebel', 'floor':'1', 'room_number':'SC 1103', 'room_type':'sleep room' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'1', 'room_number':'SC 1105', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'1', 'room_number':'SC 1109', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'1', 'room_number':'SC 1112', 'room_type':'game room' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'1', 'room_number':'SC 1131', 'room_type':'game room' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'1', 'room_number':'SC 1214', 'room_type':'sleep room' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'1', 'room_number':'SC 1302', 'room_type':'Interactive Intelligence room' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'1', 'room_number':'SC 1304', 'room_type':'Special sponsor room' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'1', 'room_number':'SC 1404', 'room_type':'Kickoff and Tech Talks' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'1', 'room_number':'SC Atrium', 'room_type':'sponsor tables and food serving' ,'room_name':'1st Floor Atrium', 'image_url':''},
+{'building':'Siebel', 'floor':'1', 'room_number':'SC 1312', 'room_type':'Mission Control Annex' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'2', 'room_number':'SC 2405', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'2', 'room_number':'SC 2235', 'room_type':'sponsor interview' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'2', 'room_number':'SC 2237', 'room_type':'sponsor interview' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'2', 'room_number':'SC 2nd Atrium', 'room_type':'sponsor tables' ,'room_name':'2nd floor Atrium', 'image_url':''},
+{'building':'Siebel', 'floor':'2', 'room_number':'SC 2102', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'2', 'room_number':'SC 2124', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'2', 'room_number':'SC 2407', 'room_type':'Mission Control' ,'room_name':'Mission Control', 'image_url':''},
+{'building':'Siebel', 'floor':'3', 'room_number':'SC 3401', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'3', 'room_number':'SC 3403', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'3', 'room_number':'SC 3405', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'3', 'room_number':'SC 3102', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'3', 'room_number':'SC 3124', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'4', 'room_number':'SC 4102', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'4', 'room_number':'SC 4124', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'4', 'room_number':'SC 4403', 'room_type':'sponsor room' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'4', 'room_number':'SC 4405', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'Siebel', 'floor':'4', 'room_number':'SC 4407', 'room_type':'sponsor room' ,'room_name':'', 'image_url':''},
+{'building':'DCL', 'floor':'1', 'room_number':'DCL 1310', 'room_type':'tech talks' ,'room_name':'', 'image_url':''},
+{'building':'DCL', 'floor':'1', 'room_number':'DCL 1320', 'room_type':'hacker space' ,'room_name':'', 'image_url':''},
+{'building':'DCL', 'floor':'2', 'room_number':'DCL 2320', 'room_type':'hardware hacker space' ,'room_name':'', 'image_url':''},
+{'building':'DCL', 'floor':'2', 'room_number':'DCL 2436', 'room_type':'hardware hacker space' ,'room_name':'', 'image_url':''}];
+
+
 $(document).ready(function() {
 
 
@@ -52,17 +89,32 @@ $(document).ready(function() {
 
   //RELEASE THE BLOODHOUND!!
   var companiesHound = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    datumTokenizer: function(d) {
+      return Bloodhound.tokenizers.whitespace(d.value);
+    },
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    local: $.map(companyNames, function(c) { return { value: c }; })
+    local: companyNames.map(function(c) {
+      return { value: c };
+    })
+  });
+
+  var roomsHound = new Bloodhound({
+    datumTokenizer: function(d) {
+      return Bloodhound.tokenizers.whitespace(d.value);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: rooms.map(function(r){
+      return { value: r['room_number']};
+    })
   });
 
 // kicks off the loading/processing of `local` and `prefetch`
 companiesHound.initialize();
+roomsHound.initialize();
 
-$("input[name='company']").typeahead({
-  hint: true,
-  highlight: true,
+$("#bloodhound input[name='company']").typeahead({
+  hint: false,
+  highlight: false,
   minLength: 1
 },
 {
@@ -72,33 +124,67 @@ $("input[name='company']").typeahead({
   // is compatible with the typeahead jQuery plugin
   source: companiesHound.ttAdapter()
 });
+$("#bloodhound input[name='location']").typeahead({
+  hint: false,
+  highlight: false,
+  minLength: 1
+},
+{
+  name: 'room',
+  displayKey: 'value',
+  // `ttAdapter` wraps the suggestion engine in an adapter that
+  // is compatible with the typeahead jQuery plugin
+  source: roomsHound.ttAdapter()
+});
+
 
 
     // Character Counter for Description Box
     $("#inputDescription").on("change paste keydown keyup", function() {
-      $("#char-count").text($("#inputDescription").val().length)
+      $("#char-count").text($("#inputDescription").val().length);
     });
-
-
-
 
 
 
     $("#render-feeditem").on('click', function(){
 
+      var obj = parseForm();
+      var card = createCard(obj);
+
+      $(".feed-container > ul").prepend(card);
+    });
+
+
+    $("button[type='submit']").click(function(){
+      //send post
+      $.post({
+        data: parseForm(),
+        url: '',
+        success: function(data, status){
+          alert(status);
+          //clear form fields
+          $("input[name='eventName']").val('');
+          obj.description = $("textarea[name='description']").val('');
+          obj.location = $("input[name='location']").val('');
+        }
+      });
+
+      //clear fields
+
+    });
+
+    function parseForm(){
       var obj = {};
       obj.event_name = $("input[name='eventName']").val();
       obj.description = $("textarea[name='description']").val();
       obj.location = $("input[name='location']").val();
-      obj.img_src = getTypeUrl($("input[name='type']:checked"));
+      obj.img_src = getTypeUrl($("input[name='company']"));
       obj.day = getTypeUrl($("input[name='day']:checked"));
+      obj.time = moment().format('HH:mm:ss');
 
-      //debugger;
-      var card = createCard(obj);
+      return obj;
+    }
 
-      $(".feed-container > ul").prepend(card);
-      debugger;
-    });
 
   });
 
@@ -106,12 +192,10 @@ function getTypeUrl(field){
 
   var def = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjZWVlIj48L3JlY3Q+PHRleHQgdGV4dC1hbmNob3I9Im1pZGRsZSIgeD0iMzIiIHk9IjMyIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjEycHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NjR4NjQ8L3RleHQ+PC9zdmc+";
   //var def = "http://www.hackillinois.org/img/icons-iOS/hackillinois.png";
-  var types = {
-    "emergency" : "http://www.hackillinois.org/img/icons-iOS/emergency.png",
-    "announcment" : "http://www.hackillinois.org/img/icons-iOS/announce.png",
-  };
-
-  return types[field.val()] || def;
+  if( !field.val() ){
+    return def;
+  }
+  else return "/img/icons-iOS/" + field.val().toLowerCase() + ".png"
 }
 
 
@@ -121,7 +205,7 @@ function createCard(obj){
   var li = $("<li class='row event card'>");
   var img_sec = $("<section class='icon col-md-1'>");
   var img = $("<img class='media-object' style='width: 64px; height: 64px;''>");
-  img.attr('src', obj.image_src)
+  img.attr('src', obj.img_src);
   img_sec.append(img);
 
   var main_section = $("<section class='main-content col-md-6'>");
--- handlers/AdminHandlers/ScheduleHandler.py
+++ handlers/AdminHandlers/ScheduleHandler.py
@@ -12,22 +12,8 @@ class ScheduleHandler(MainAdminHandler.BaseAdminHandler):
         if not admin_user.mobileAccess:
             return self.abort(401, detail='User does not have permission to update the mobile newsfeed.')
 
-        feed = []
 
-        for num in range(10):
-          feed.append(
-          {
-          "event_name" : "A fine Event {}".format(num) ,
-          "description" : "kajsdnfkansdfjkansdjkfnaskjdfnasjkdfnjasdnf",
-          "location" : "1404 SC",
-          "time" : time.time(),
-          "icon_url" : "http://www.hackillinois.org/img/icons-iOS/emergency.png",
-          "day" : "Friday"
-          }
-          )
-
-        #items = Schedule.query(ancestor=Schedule.get_default_event_parent_key()).order(-Schedule.time)
-
+        feed = Schedule.query(ancestor=Schedule.get_default_event_parent_key()).order(-Schedule.time)
 
         #print feed
         return self.render("admin_mobile_schedule.html", feed=feed, access=json.loads(admin_user.access))
@@ -40,32 +26,15 @@ class ScheduleHandler(MainAdminHandler.BaseAdminHandler):
             return self.abort(401, detail='User does not have permission to update the mobile newsfeed.')
 
 
-        icon = {
-            'emergency':'http://www.hackillinois.org/img/icons-iOS/emergency.png',
-            'announcement':'http://www.hackillinois.org/img/icons-iOS/announce.png',
-            'hackillinois':'http://www.hackillinois.org/img/icons-iOS/hackillinois.png',
-        }
-
-        colors = {
-            'emergency':{'r':167, 'g':65, 'b':46},
-            'announcement':{'r':165, 'g':165, 'b':165},
-            'hackillinois':{'r':27, 'g':77, 'b':104},
-        }
 
         #check all fields valid
-        event_name = self.request.get('event_name')
-        if not event_name or event_name == "":
-          return self.write(json.dumps({'message':'You Must Supply an Event Name'}))
-
-        description = self.request.get("description")
-
 
         Schedule(
-            event_name=NewsFeedItem.get_default_event_parent_key(),
-            description=description,
+            event_name=self.request.get('event_name'),
+            description=self.request.get('description'),
             location=self.request.get("location"),
             time=int(time.time()),
-            icon_url=icon[str(urllib.unquote(self.request.get("type")))],
+            icon_url= self.request.get('img_src'),
             day = str(urllib.unquote(self.request.get('day')))
         ).put()
 
