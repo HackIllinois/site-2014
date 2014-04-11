a34032f8912511c0678a758e22e44df2f6507174
bjryan2 <bjryan2@illinois.edu>
2014-04-11 00:36:33 -0500

    render of preview events works

--- assets/js/admin_schedule.js
+++ assets/js/admin_schedule.js
@@ -12,23 +12,24 @@ $(document).ready(function() {
 
       var obj = {};
       obj.event_name = $("input[name='eventName']").val();
-      obj.description = $("input[name='description']").val();
-      obj.event_loc = $("input[name='eventLocation']").val();
+      obj.description = $("textarea[name='description']").val();
+      obj.location = $("input[name='location']").val();
       obj.img_src = getTypeUrl($("input[name='type']:checked"));
-      obj.img_src = getTypeUrl($("input[name='day']:checked"));
+      obj.day = getTypeUrl($("input[name='day']:checked"));
 
-
-
-
+      //debugger;
       var card = createCard(obj);
-      $("feed-container > ul").append(card);
+
+      $(".feed-container > ul").prepend(card);
+      debugger;
     });
 
   });
 
 function getTypeUrl(field){
 
-  var def = "http://www.hackillinois.org/img/icons-iOS/hackillinois.png";
+  var def = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjZWVlIj48L3JlY3Q+PHRleHQgdGV4dC1hbmNob3I9Im1pZGRsZSIgeD0iMzIiIHk9IjMyIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjEycHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NjR4NjQ8L3RleHQ+PC9zdmc+";
+  //var def = "http://www.hackillinois.org/img/icons-iOS/hackillinois.png";
   var types = {
     "emergency" : "http://www.hackillinois.org/img/icons-iOS/emergency.png",
     "announcment" : "http://www.hackillinois.org/img/icons-iOS/announce.png",
@@ -48,18 +49,16 @@ function createCard(obj){
   img_sec.append(img);
 
   var main_section = $("<section class='main-content col-md-6'>");
-  var event_name = $("<p>").text(obj.event_name);
-  var desc = $("<p>").text(obj.description);
-  main_section.append(event_name);
-  main_section.append(desc);
+  var event_name = $("<p class='event-name'>").text(obj.event_name);
+  var desc = $("<p class='description'>").text(obj.description);
+  main_section.append(event_name).append(desc);
 
   var log_section = $("<section class='logistics col-md-2 pull-right'>")
 
   var location = $("<p>").text(obj.location);
   var time = $("<p>").text(obj.time);
 
-  log_section.append(location);
-  log_section.append(time);
+  log_section.append(location).append(time);
 
   li.append(img_sec).append(main_section).append(log_section);
   return li;
