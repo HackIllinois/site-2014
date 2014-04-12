2ea13ccc515c916a4751c9303371600fdb0f917c
Matthew Dierker <mvd7793@gmail.com>
2014-04-12 11:39:02 -0500

    new way of texting all attendees so it hits more than 10% of people ;)

--- handlers/TwilioHandlers.py
+++ handlers/TwilioHandlers.py
@@ -1,3 +1,4 @@
+import json
 import MainHandler
 from db import constants
 from db.PhoneNumber import PhoneNumber
@@ -5,8 +6,10 @@ import logging
 from twilio.rest import TwilioRestClient
 import twilio.twiml
 
+
 class TwilioHandler(MainHandler.Handler):
     client = None
+
     def dispatch(self):
         self.client = TwilioRestClient(constants.TWILIO_ACCOUNT_SID, constants.TWILIO_AUTH_TOKEN)
         super(TwilioHandler, self).dispatch()
@@ -21,11 +24,11 @@ class MassTextHandler(TwilioHandler):
         if not body: self.abort(400, detail="No 'Body' parameter given.")
 
         q = PhoneNumber.search_database({"groups": "SendStaff"})
-        authorized_senders = [ i.number for i in q ]
+        authorized_senders = [i.number for i in q]
 
         if from_number in authorized_senders:
             q = PhoneNumber.search_database({"groups": "Staff"})
-            group_numbers = [ i.number for i in q ]
+            group_numbers = [i.number for i in q]
 
             for number in group_numbers:
                 message = self.client.sms.messages.create(
@@ -53,11 +56,11 @@ class TextEveryoneHandler(TwilioHandler):
         if not body: self.abort(400, detail="No 'Body' parameter given.")
 
         q = PhoneNumber.search_database({"groups": "SendStaff"})
-        authorized_senders = [ i.number for i in q ]
+        authorized_senders = [i.number for i in q]
 
         if from_number in authorized_senders:
             q = PhoneNumber.search_database({"groups": "Staff"})
-            group_numbers = [ i.number for i in q ]
+            group_numbers = [i.number for i in q]
 
             # Send to all staff
             for number in group_numbers:
@@ -84,3 +87,25 @@ class TextEveryoneHandler(TwilioHandler):
                 from_="+17864225451"
             )
             logging.info("Unauthorized user tried to send a message: %s" % from_number)
+
+
+class AllNumbersHandler(MainHandler.Handler):
+    def get(self):
+        q = PhoneNumber.search_database({"groups": "Staff"})
+        group_numbers = [i.number for i in q]
+
+        numbers_to_show = []
+
+        # Send to all staff
+        for number in group_numbers:
+            numbers_to_show.append(number)
+
+        # Send to all Attendees
+        for number in constants.ATTENDEE_NUMBERS:
+            numbers_to_show.append(number)
+
+        out = {
+            'phone_numbers': numbers_to_show
+        }
+
+        self.write(json.dumps(out))
