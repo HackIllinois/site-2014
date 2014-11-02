a5912cecff33974a55b6f5130ba7122ab089390d
Ben Fishbein <Ben@hackillinois.org>
2014-11-02 09:35:16 -0500

    changed app

--- appengine_config.py
+++ appengine_config.py
@@ -29,6 +29,8 @@ sys.path.append(os.path.join(third_party, 'twilio'))
 # https://developers.google.com/appengine/docs/python/tools/appstats?csw=1
 appstats_CALC_RPC_COSTS = True
 
+remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = ('HTTP_X_APPENGINE_INBOUND_APPID',['hackillinois'])
+
 def webapp_add_wsgi_middleware(app):
       from google.appengine.ext.appstats import recording
       app = recording.appstats_wsgi_middleware(app)
